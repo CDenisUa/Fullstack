ed917c605763722c648e29171d5f796c90664f51
Denys <chepigadeveloper@gmail.com>
2024-11-20 21:52:07 +0100

    x-social-network/Add followUnfollowUser controller, added /follow/:id route

--- x-social-network/backend/src/routes/user.routes.js
+++ x-social-network/backend/src/routes/user.routes.js
@@ -3,13 +3,16 @@ import express from 'express';
 // Middleware
 import {protectRoute} from "#src/middleware/protectRoute.js";
 // Controllers
-import { getUserProfile } from "#controllers/user.controller.js";
+import {
+    getUserProfile,
+    followUnfollowUser,
+} from "#controllers/user.controller.js";
 
 const router = express.Router();
 
 router.get('/profile/:userName',protectRoute, getUserProfile);
 // router.get('/suggested',protectRoute, getUserProfile);
-// router.post('/follow/:id',protectRoute, followUnfollowUser);
+router.post('/follow/:id',protectRoute, followUnfollowUser);
 // router.post('/update',protectRoute, updateUserProfile);
 
 export default router;
