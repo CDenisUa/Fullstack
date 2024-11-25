55de91a9f2a5d9a5fcea60858a037468f96ceff0
Denys <chepigadeveloper@gmail.com>
2024-11-25 21:34:31 +0100

    x-social-network/Updated Readme.md file

--- x-social-network/backend/server.js
+++ x-social-network/backend/server.js
@@ -3,7 +3,7 @@ import express from 'express';
 // DB
 import connectToDatabase from '#root/db.js';
 // Middlewares
-import loadMiddleware from '#src/middleware/index.js';
+import loadMiddlewares from '#src/middleware/index.js';
 // Routes
 import authRoutes from '#routes/auth.routes.js';
 import userRoutes from '#routes/user.routes.js';
@@ -11,7 +11,7 @@ import userRoutes from '#routes/user.routes.js';
 const PORT = process.env.PORT || 4200;
 const app = express();
 
-loadMiddleware(app);
+loadMiddlewares(app);
 
 // Routes
 app.use('/api/auth', authRoutes);
