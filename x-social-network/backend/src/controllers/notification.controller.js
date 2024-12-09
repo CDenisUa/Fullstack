3b50cac7fd12cdb565512dce0411a9b2fb385797
Denys <chepigadeveloper@gmail.com>
2024-12-09 23:17:33 +0100

    x-social-network/Created deleteNotification controller

--- x-social-network/backend/src/controllers/notification.controller.js
+++ x-social-network/backend/src/controllers/notification.controller.js
@@ -20,5 +20,13 @@ export const getNotifications = async (req, res) => {
 }
 
 export const deleteNotifications = async (req, res) => {
+    try {
+        const userId = req.user['_id'];
+        await Notification.deleteMany({ to: userId });
 
+        res.status(200).json({message: "Notification deleted successfully."});
+    } catch (error) {
+        console.log('Error in deleteNotifications: ', error.message);
+        res.status(500).json({ error: error.message || 'Error in deleteNotifications' });
+    }
 }
