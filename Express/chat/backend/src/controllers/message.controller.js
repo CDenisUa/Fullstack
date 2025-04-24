4a47033acb2dfd4f885365c581b6a990b17cb30d
Denys Chepiha <dchepiha@exoft.net>
2025-04-24 14:02:01 +0200

    chat: Implemented sentMessage endpoint

--- Express/chat/backend/src/controllers/message.controller.js
+++ Express/chat/backend/src/controllers/message.controller.js
@@ -1,6 +1,7 @@
 // Models
 import User from '#models/user.model.js'
 import Message from "#models/message.model.js";
+import cloudinary from "#lib/cloudinary.js";
 
 export const getUsersForSidebar = async (req, res) => {
     const loggedInUserId = req.user['_id'];
@@ -35,6 +36,34 @@ export const getMessages = async (req, res) => {
         res.status(200).json(messages);
     } catch (error) {
         console.log("Error in getMessages", error.message);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+}
+
+export const sentMessage = async (req, res) => {
+    const { text, image } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user['_id'];
+    let imageUrl;
+
+    try {
+        if(image) {
+            const uploadResponse = await cloudinary.uploader.upload(image);
+            imageUrl = uploadResponse.secure_url;
+        }
+
+        const newMessage = new Message({
+            senderId,
+            receiverId,
+            text,
+            image: imageUrl,
+        });
+
+        await newMessage.save();
+
+        res.status(200).json(newMessage);
+    } catch (error) {
+        console.log("Error in sentMessage", error.message);
         res.status(500).json({ error: 'Internal Server Error' });
     }
 }
--- Express/chat/backend/src/routes/message.route.js
+++ Express/chat/backend/src/routes/message.route.js
@@ -3,11 +3,16 @@ import express from "express";
 // Middleware
 import {protectRoute} from "#middleware/auth.middleware.js";
 // Controllers
-import { getUsersForSidebar } from "#controllers/message.controller.js"
+import {
+    sentMessage,
+    getMessages,
+    getUsersForSidebar
+} from "#controllers/message.controller.js"
 
 const router = express.Router();
 
 router.get('/users', protectRoute, getUsersForSidebar)
 router.get('/:id', protectRoute, getMessages)
+router.post('/send/:id', protectRoute, sentMessage);
 
 export default router;
