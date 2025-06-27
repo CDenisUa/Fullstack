2ecdaf4e0f324192a31e4afc6766069e0f557d4f
Denys Chepiha <dchepiha@exoft.net>
2025-06-27 21:14:14 +0200

    chat: Configured Tailwind, daisyUI

--- Express/chat/frontend/src/main.tsx
+++ Express/chat/frontend/src/main.tsx
@@ -1,6 +1,9 @@
+// Core
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+// Styles
 import './index.css'
+// Components
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
